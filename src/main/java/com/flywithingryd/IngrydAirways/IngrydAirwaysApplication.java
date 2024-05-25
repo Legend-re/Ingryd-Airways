54cb5cafcf5441a760c3be38132aae775d4167c8
legend-re <ajiamubarak3@gmail.com>
2024-05-25 15:56:30 +0100

    default

---
+++ src/main/java/com/flywithingryd/IngrydAirways/IngrydAirwaysApplication.java
@@ -0,0 +1,13 @@
+package com.flywithingryd.IngrydAirways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngrydAirwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngrydAirwaysApplication.class, args);
+	}
+
+}
